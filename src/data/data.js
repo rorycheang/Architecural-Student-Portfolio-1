4213b089ce750b292dc100ed165d2cac4c0c8f18
rorycheang <roryiocheang@gmail.com>
2025-01-15 17:31:08 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Rory Cheang",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "rory.cheang@ucalgary.ca",
+        phone: "+13068077106",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
